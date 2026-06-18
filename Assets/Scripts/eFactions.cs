b72f16d7a7a3c8db2cf76976e78a14088fbd282d
Selshas Ranos <selshas@gmail.com>
2026-06-18 21:37:28 +0900

    Factions - updated to dow4 spec

--- Assets/Scripts/eFactions.cs
+++ Assets/Scripts/eFactions.cs
@@ -1,12 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public enum eFaction : int
 {
     None = -1,
 
-    GDF = 0,
-    TD = 1,
-    Veti = 2
+    SpaceMarine = 0,
+    Ork = 1,
+    Necron = 2,
+    AdeptusMechanicus = 3
 }
